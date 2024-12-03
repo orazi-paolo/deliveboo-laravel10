4e8ae5d07ed07598bec05ec038f93b0ea27aeff4
orsoli <fljcoli@gmail.com>
2024-12-03 14:04:08 +0100

    Modify create or edit plates view.

--- resources/views/admin/plates/layouts/create-or-edit.blade.php
+++ resources/views/admin/plates/layouts/create-or-edit.blade.php
@@ -8,7 +8,7 @@
         @yield('form-method')
 
         <h2 class="text-decoration-underline text-center mb-3">@yield('form-title')</h2>
-        <div class="row mb-3">
+        {{-- <div class="row mb-3">
             <div class="col-12 col-lg-6">
                 <label for="image" class="form-label">Choose Image</label>
                 <input type="file" class="form-control" name="image" id="image" value="{{old('name', $plate->image)}}">
@@ -16,7 +16,7 @@
                 <small><i class="text-danger">{{$message}}</i></small>
                 @enderror
             </div>
-        </div>
+        </div> --}}
         <div class="row mb-3">
             <div class="col-12 col-lg-6">
                 <label for="name" class="form-label">Plate Name</label>
@@ -28,9 +28,9 @@
         </div>
         <div class="row mb-3">
             <div class="col-12">
-                <label for="description" class="form-label">Description</label>
-                <textarea class="form-control" name="description" id="description"
-                    rows="3">{{old('description', $plate->description)}}</textarea>
+                <label for="ingredient_description" class="form-label">Ingredients Description</label>
+                <textarea class="form-control" name="ingredient_description" id="ingredient_description"
+                    rows="3">{{old('ingredient_description', $plate->ingredient_description)}}</textarea>
                 @error('description')
                 <small><i class="text-danger">{{$message}}</i></small>
                 @enderror
@@ -38,10 +38,30 @@
         </div>
         <div class="row mb-3">
             <div class="col-12">
-                <label for="ingredient_description" class="form-label">Ingredients Description</label>
-                <textarea class="form-control" name="ingredient_description" id="ingredient_description"
-                    rows="3">{{old('ingredient_description', $plate->ingredient_description)}}</textarea>
-                @error('description')
+                <label for="description" class="form-label">Description</label>
+                <textarea class="form-control" name="description" id="description"
+                    rows="3">{{old('description', $plate->description)}}</textarea>
+                @error('ingredient_description')
+                <small><i class="text-danger">{{$message}}</i></small>
+                @enderror
+            </div>
+        </div>
+        <div class="row mb-3">
+            <div class="col-12 col-lg-6">
+                <label for="price" class="form-label">Price</label>
+                <input type="number" class="form-control" name="price" id="price"
+                    value="{{old('price', $plate->price)}}">
+                @error('price')
+                <small><i class="text-danger">{{$message}}</i></small>
+                @enderror
+            </div>
+        </div>
+        <div class="row mb-3">
+            <div class="col-12 col-lg-6">
+                <input type="checkbox" name="visible" class="form-check-input"
+                    value="{{old('visible', $plate->visible)}}" id="visible">
+                <label for="visible" class="form-check-label">Visible</label>
+                @error('visible')
                 <small><i class="text-danger">{{$message}}</i></small>
                 @enderror
             </div>
