266dfec5cb1276499d2e18413299a1cf22e22d8b
Marcap00 <cappellatomarco@yahoo.com>
2024-12-03 14:24:34 +0100

    make migration for plates table database

---
+++ database/migrations/2024_12_03_125910_create_plates_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('plates', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId("restaurant_id")->constrained()->cascadeOnUpdate()->cascadeOnDelete();
+            $table->string("name")->nullable(false);
+            $table->text("description")->nullable();
+            $table->text("ingredient_description")->nullable();
+            $table->decimal("price", 8, 2, true)->nullable(false);
+            $table->boolean("visible")->default(false)->nullable(false);
+            $table->text("image")->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('plates');
+    }
+};
