44deb607180973eba73cffaaba203ce94bb38320
Marcap00 <cappellatomarco@yahoo.com>
2024-12-03 22:59:31 +0100

    add TipologySeeder in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UserSeeder::class,
-            RestaurantSeeder::class
+            RestaurantSeeder::class,
+            TipologySeeder::class
         ]);
     }
 }
