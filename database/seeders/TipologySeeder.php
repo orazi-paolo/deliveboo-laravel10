14586ebdb5d524abe68303a0b3f8e73832df9dfe
Marcap00 <cappellatomarco@yahoo.com>
2024-12-03 22:58:08 +0100

    created TipologySeeder and populated with created array of tipology

---
+++ database/seeders/TipologySeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Tipology;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TipologySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $tipologies = [
+            "Pizza",
+            "Hamburger",
+            "Sushi",
+            "Cucina Italiana",
+            "Cucina Cinese",
+            "Cucina Indiana",
+            "Cucina Giapponese",
+            "Cucina Messicana",
+            "Cucina Mediterranea",
+            "Cucina Vegetariana",
+            "Cucina Vegana",
+            "Healthy",
+            "Fast Food",
+            "Pasticceria",
+            "Gelato",
+        ];
+
+        foreach ($tipologies as $tipology) {
+            Tipology::create([
+                'name' => $tipology,
+                "image" => "https://placehold.co/600x400?text=Tipology"
+            ]);
+        }
+    }
+}
