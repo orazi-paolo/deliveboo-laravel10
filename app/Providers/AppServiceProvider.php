56456c992e90907402c32f1f4b8ee3778429af0b
orazi-paolo <orazi.paolo10@gmail.com>
2024-12-03 14:09:40 +0100

    managed email sending

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Auth\Notifications\ResetPassword;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,18 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Personalizza l'email di reset della password
+        ResetPassword::toMailUsing(function ($notifiable, $token) {
+            // Crea l'URL di reset della password
+            $url = url(route('password.reset', [
+                'token' => $token,
+                'email' => $notifiable->getEmailForPasswordReset(),
+            ], false));
+
+            // Usa la vista personalizzata
+            return (new \Illuminate\Notifications\Messages\MailMessage)
+                ->subject('Reset your password')
+                ->view('emails.reset-password', ['url' => $url]);
+        });
     }
 }
