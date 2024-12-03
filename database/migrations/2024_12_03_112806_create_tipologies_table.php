db8360aeccbf62c55caa9f781f33718202141128
Marcap00 <cappellatomarco@yahoo.com>
2024-12-03 12:42:29 +0100

    add soft deletes to tipologies table

--- database/migrations/2024_12_03_112806_create_tipologies_table.php
+++ database/migrations/2024_12_03_112806_create_tipologies_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string("name")->nullable("false");
             $table->text("image")->nullable();
+            $table->softDeletes();
             $table->timestamps();
         });
     }
