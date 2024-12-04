eb514849eae0a34ac946cc8825d2d1d5cdb5d38a
orazi-paolo <orazi.paolo10@gmail.com>
2024-12-04 12:28:51 +0100

    when you open the page it redirects you immediately to the registration page

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('register');
 });
 
 Auth::routes(['verify' => true]);
