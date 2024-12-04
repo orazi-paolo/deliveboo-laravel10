9c8875aca4afa6c72d4b7b420e64de5055727952
orazi-paolo <orazi.paolo10@gmail.com>
2024-12-04 15:49:26 +0100

    implemented the home page with the restaurant

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -45,4 +45,43 @@
         @endif
     </div>
 </div>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header bg-header fw-bold turquoise">{{ __('Your restaurant details') }}</div>
+
+                <div class="card-body">
+                    <div class="row mb-3 justify-content-center">
+                        <div class="col-md-6 d-flex justify-content-center">
+                            <img src="{{ asset('storage/' . $restaurant->image) }}" alt="{{ $restaurant->name }}" class="img-fluid">
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <label class="col-md-4 col-form-label text-md-end turquoise">{{ __('Name') }}</label>
+                        <div class="col-md-6">
+                            <p class="form-control-plaintext">{{ $restaurant->name }}</p>
+                        </div>
+                        <label class="col-md-4 col-form-label text-md-end turquoise">{{ __('Description') }}</label>
+                        <div class="col-md-6">
+                            <p class="form-control-plaintext">{{ $restaurant->description }}</p>
+                        </div>
+                        <label class="col-md-4 col-form-label text-md-end turquoise">{{ __('Address') }}</label>
+                        <div class="col-md-6">
+                            <p class="form-control-plaintext">{{ $restaurant->address }}</p>
+                        </div>
+                        <label class="col-md-4 col-form-label text-md-end turquoise">{{ __('City') }}</label>
+                        <div class="col-md-6">
+                            <p class="form-control-plaintext">{{ $restaurant->city }}</p>
+                        </div>
+                        <label class="col-md-4 col-form-label text-md-end turquoise">{{ __('VAT') }}</label>
+                        <div class="col-md-6">
+                            <p class="form-control-plaintext">{{ $restaurant->VAT }}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
