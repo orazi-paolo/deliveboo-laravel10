53ef7e109d5c2aee8f378b89143bd7b4bd1f4926
orazi-paolo <orazi.paolo10@gmail.com>
2024-12-04 11:46:21 +0100

    create custom request for restaurants

---
+++ app/Http/Requests/StoreRestaurantRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreRestaurantRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return auth()->check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'image' => 'nullable|image|max:2048',
+            'address' => 'required|string|max:255',
+            'city' => 'required|string|max:255',
+            'VAT' => 'required|string|size:11|unique:restaurants,VAT',
+        ];
+    }
+}
