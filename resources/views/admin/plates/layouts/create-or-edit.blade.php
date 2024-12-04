8c44fe668f06b622d3e3f374116bc07fd3092d94
Marcap00 <cappellatomarco@yahoo.com>
2024-12-04 20:37:18 +0100

    add required to inputs form of create-or-edit plates

--- resources/views/admin/plates/layouts/create-or-edit.blade.php
+++ resources/views/admin/plates/layouts/create-or-edit.blade.php
@@ -28,7 +28,7 @@
                 <div class="col-12 col-lg-6">
                     <label for="name" class="form-label turquoise">Plate Name</label>
                     <input type="text" class="form-control" name="name" id="name"
-                        value="{{ old('name', $plate->name) }}">
+                        value="{{ old('name', $plate->name) }}" required>
                     @error('name')
                         <small><i class="text-danger">{{ $message }}</i></small>
                     @enderror
@@ -56,7 +56,7 @@
                 <div class="col-12 col-lg-6">
                     <label for="price" class="form-label turquoise">Price</label>
                     <input type="number" step="0.01" class="form-control" name="price" id="price"
-                        value="{{ old('price', $plate->price) }}">
+                        value="{{ old('price', $plate->price) }}" required>
                     @error('price')
                         <small><i class="text-danger">{{ $message }}</i></small>
                     @enderror
