b56f5a46904634ca95ec6f65f8ae563a07e60552
orazi-paolo <orazi.paolo10@gmail.com>
2024-12-04 11:46:56 +0100

    created the controller for restaurants

---
+++ app/Http/Controllers/Admin/RestaurantController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\Restaurant;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Storage;
+use App\Http\Requests\StoreRestaurantRequest;
+
+class RestaurantController extends Controller
+{
+    public function create()
+    {
+        return view('admin.restaurants.create');
+    }
+
+    public function store(StoreRestaurantRequest $request)
+    {
+        $validated = $request->validated();
+
+        if ($request->hasFile('image')) {
+            $filepath = Storage::disk('public')->put('images/restaurants', $request->file('image'));
+            $validated['image'] = $filepath;
+        }
+
+        Restaurant::create([
+            'user_id' => auth()->id(),
+            'name' => $validated['name'],
+            'description' => $validated['description'] ?? null,
+            'image' => $validated['image'] ?? null,
+            'address' => $validated['address'],
+            'city' => $validated['city'],
+            'VAT' => $validated['VAT'],
+        ]);
+
+        return redirect()->route('home')->with('success', 'Restaurant has been created successfully!');
+    }
+}
