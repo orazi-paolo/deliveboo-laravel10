a0b053cbf19d6549ac75ac085637643804e1e990
orazi-paolo <orazi.paolo10@gmail.com>
2024-12-05 17:28:29 +0100

    successful registration message in login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,6 +7,12 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
+            @if(session('success'))
+            <div class="alert alert-success alert-dismissible fade show" role="alert">
+                {{ session('success') }}
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+            @endif
             <div class="card">
                 <div class="card-header bg-header fw-bold">{{ __('Login') }}</div>
 
