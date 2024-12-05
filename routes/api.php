22eeb7048ca1c995d29bffcd326b96bb6eaca0b0
Marcap00 <cappellatomarco@yahoo.com>
2024-12-05 23:15:05 +0100

    created routes api for technologies

--- routes/api.php
+++ routes/api.php
@@ -20,11 +20,18 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// Plate resources
+// Plate API resources
 Route::prefix("plates")->name("api.plates.")->group(function () {
     Route::get("/", [PlateController::class, "index"])->name("index");
     Route::get("/{plate}", [PlateController::class, "show"])->name("show");
 });
-
-Route::get('/restaurant', [RestaurantController::class, 'index'])->name('api.restaurants.index');
-Route::get('/restaurant/{restaurant}', [RestaurantController::class, 'show'])->name('api.restaurants.show');
+// Restaurant API resources
+Route::prefix("restaurant")->name("api.restaurants.")->group(function () {
+    Route::get('/', [RestaurantController::class, 'index'])->name('index');
+    Route::get('/{restaurant}', [RestaurantController::class, 'show'])->name('show');
+});
+// Tipology API resources
+Route::prefix("tipologies")->name("api.tipologies.")->group(function () {
+    Route::get('/', [RestaurantController::class, 'index'])->name('index');
+    Route::get('/{tipology}', [RestaurantController::class, 'show'])->name('show');
+});
