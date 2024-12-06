bb781f512d4155052ef54f3fd4e4e55880449414
Marcap00 <cappellatomarco@yahoo.com>
2024-12-06 21:01:59 +0100

    fixed image on show

--- resources/views/admin/plates/show.blade.php
+++ resources/views/admin/plates/show.blade.php
@@ -1,34 +1,40 @@
 @extends('layouts.app')
 
 @section('scss')
-@vite('resources/sass/plate-show.scss')
+    @vite('resources/sass/plate-show.scss')
 @endsection
 
 @section('content')
-<main>
-    <div class="container">
+    <main>
+        <div class="container">
 
-        <a href="{{ route('admin.plates.index') }}" class="text-decoration-none text-secondary">
-            <i class="bi bi-arrow-left"></i>
-            Back
-        </a>
+            <a href="{{ route('admin.plates.index') }}" class="text-decoration-none text-secondary">
+                <i class="bi bi-arrow-left"></i>
+                Back
+            </a>
 
-        <div class="row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-3 my-5 g-5">
-            <div class="col">
-                <img src="{{asset('/storage/'. $plate->image)}}" class="rounded-4 shadow">
-            </div>
-            <div class="col">
-                <h1 class="fw-bold">{{strtoupper($plate->name)}}</h1>
-                <p>
-                    <span class="fw-bold fst-italic text-wrap turquoise">Ingredients:</span>
-                    {{$plate->ingredient_description}}
-                </p>
-                <p>
-                    <span class="fw-bold fst-italic turquoise">Description:</span> {{$plate->description}}
-                </p>
+            <div class="row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-3 my-5 g-5">
+                <div class="col">
+                    @if ($plate->image)
+                        <img src="{{ asset('/storage/' . $plate->image) }}" alt="{{ $plate->name . '\'s image' }}"
+                            class="plate-image rounded-2 shadow my-2">
+                    @else
+                        <img src="{{ $plate->image_placeholder }}" alt="{{ $plate->name . '\'s image' }}"
+                            class="plate-image rounded-2 shadow my-2">
+                    @endif
+                </div>
+                <div class="col">
+                    <h1 class="fw-bold">{{ strtoupper($plate->name) }}</h1>
+                    <p>
+                        <span class="fw-bold fst-italic text-wrap turquoise">Ingredients:</span>
+                        {{ $plate->ingredient_description }}
+                    </p>
+                    <p>
+                        <span class="fw-bold fst-italic turquoise">Description:</span> {{ $plate->description }}
+                    </p>
+                </div>
             </div>
         </div>
-    </div>
 
-</main>
+    </main>
 @endsection
