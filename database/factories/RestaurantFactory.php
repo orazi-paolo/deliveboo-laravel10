38bf14de041cebd96f70d80a43c08a970a895eae
Marcap00 <cappellatomarco@yahoo.com>
2024-12-07 18:25:50 +0100

    add image restaurant

--- database/factories/RestaurantFactory.php
+++ database/factories/RestaurantFactory.php
@@ -17,6 +17,19 @@ class RestaurantFactory extends Factory
      */
     public function definition(): array
     {
+        $images = [
+            "https://images.pexels.com/photos/67468/pexels-photo-67468.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            "https://images.pexels.com/photos/262978/pexels-photo-262978.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            "https://images.pexels.com/photos/460537/pexels-photo-460537.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            "https://images.pexels.com/photos/1581554/pexels-photo-1581554.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            "https://images.pexels.com/photos/1449773/pexels-photo-1449773.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            "https://images.pexels.com/photos/776538/pexels-photo-776538.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            "https://images.pexels.com/photos/1307698/pexels-photo-1307698.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            "https://images.pexels.com/photos/262047/pexels-photo-262047.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            "https://images.pexels.com/photos/1058277/pexels-photo-1058277.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            "https://images.pexels.com/photos/941861/pexels-photo-941861.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            "https://images.pexels.com/photos/260922/pexels-photo-260922.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        ];
         $userIds = User::all()->pluck("id");
         return [
             "user_id" => fake()->unique()->randomElement($userIds),
@@ -25,7 +38,8 @@ class RestaurantFactory extends Factory
             "address" => fake()->streetAddress(),
             "city" => fake()->city(),
             "VAT" => fake()->unique()->bothify("??##?#?##??"),
-            "image" => "https://placehold.co/600x400?text=Restaurant"
+            "image" => fake()->randomElement($images),
+            "image_placeholder" => "https://placehold.co/600x400?text=Restaurant"
         ];
     }
 }
