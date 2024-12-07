fae31f7837820138445c3565d622a76365e23125
Marcap00 <cappellatomarco@yahoo.com>
2024-12-07 02:10:25 +0100

    add quantity to plate purchase table

--- database/migrations/2024_12_03_144911_create_plate_purchase_table.php
+++ database/migrations/2024_12_03_144911_create_plate_purchase_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('plate_purchase', function (Blueprint $table) {
             $table->foreignId("plate_id")->constrained()->cascadeOnUpdate()->cascadeOnDelete();
             $table->foreignId("purchase_id")->constrained()->cascadeOnUpdate()->cascadeOnDelete();
+            $table->integer("quantity")->unsigned()->default(1)->nullable();
 
             $table->primary(["plate_id", "purchase_id"]);
         });
