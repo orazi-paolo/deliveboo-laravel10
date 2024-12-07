3ddafecd6defeceaa3cca0d24bbcf58ee8fbfa55
Marcap00 <cappellatomarco@yahoo.com>
2024-12-07 19:20:19 +0100

    updated image

--- database/seeders/TipologySeeder.php
+++ database/seeders/TipologySeeder.php
@@ -16,11 +16,11 @@ class TipologySeeder extends Seeder
         $tipologies = [
             ["name" => "Pizza", "image" => "https://images.pexels.com/photos/365459/pexels-photo-365459.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"],
             ["name" => "Hamburger", "image" => "https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"],
-            ["name" => "Sushi", "image" => "https://images.pexels.com/photos/1833349/pexels-photo-1833349.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"],
+            ["name" => "Sushi", "image" => "https://images.unsplash.com/photo-1499125562588-29fb8a56b5d5?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"],
             ["name" => "Cucina Italiana", "image" => "https://images.pexels.com/photos/803963/pexels-photo-803963.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"],
             ["name" => "Cucina Cinese", "image" => "https://images.pexels.com/photos/955137/pexels-photo-955137.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"],
             ["name" => "Cucina Indiana", "image" => "https://images.pexels.com/photos/1267320/pexels-photo-1267320.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"],
-            ["name" => "Cucina Giapponese", "image" => "https://images.pexels.com/photos/955137/pexels-photo-955137.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"],
+            ["name" => "Cucina Giapponese", "image" => "https://images.pexels.com/photos/1833349/pexels-photo-1833349.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"],
             ["name" => "Cucina Messicana", "image" => "https://images.pexels.com/photos/1640772/pexels-photo-1640772.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"],
             ["name" => "Cucina Mediterranea", "image" => "https://images.pexels.com/photos/1279330/pexels-photo-1279330.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"],
             ["name" => "Cucina Vegetariana", "image" => "https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"],
