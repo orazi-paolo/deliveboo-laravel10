6d0c2e6395015a73c65b18c90bf45940d3f3f662
orazi-paolo <orazi.paolo10@gmail.com>
2024-12-10 12:48:45 +0100

    changed in the registration controller when creating the restaurant the image url

--- app/Http/Controllers/RegisterAdvanceController.php
+++ app/Http/Controllers/RegisterAdvanceController.php
@@ -33,7 +33,7 @@ class RegisterAdvanceController extends Controller
             'VAT' => $validated['VAT'],
             'city' => $validated['city'],
             'description' => $validated['description'],
-            'image' => $imagePath,
+            'image' => url('storage/' . $imagePath),
             'image_placeholder' => "https://images.pexels.com/photos/1307698/pexels-photo-1307698.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
         ]);
 
