dd84385069a41fe2c52366e497fd624b7c4dfb7e
orazi-paolo <orazi.paolo10@gmail.com>
2024-12-11 13:08:00 +0100

    fix show image

--- resources/views/admin/plates/layouts/index-or-trash.blade.php
+++ resources/views/admin/plates/layouts/index-or-trash.blade.php
@@ -54,7 +54,7 @@
                     {{-- Image --}}
                     <td class="text-center">
                         @if ($plate->image)
-                        <img src="{{ $plate->getStorageImage() }}" alt="{{ $plate->name . '\'s image' }}"
+                        <img src="{{ $plate->image }}" alt="{{ $plate->name . '\'s image' }}"
                             class="plate-image rounded-2 shadow my-2">
                         @else
                         <img src="{{ $plate->image_placeholder }}" alt="{{ $plate->name . '\'s image' }}"
