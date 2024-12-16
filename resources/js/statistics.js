2bdf39dfe60daf8cdc4a3372a908fa15c5fba2ff
Marcap00 <cappellatomarco@yahoo.com>
2024-12-16 18:50:06 +0100

    improve starts

--- resources/js/statistics.js
+++ resources/js/statistics.js
@@ -1,5 +1,5 @@
 import Chart from 'chart.js/auto';
-
+/* import { Utils } from 'chart.js/helpers'; */
 document.addEventListener('DOMContentLoaded', () => {
     const data = JSON.parse(document.querySelector('meta[name="statistics"]').content);
 
@@ -36,12 +36,106 @@ document.addEventListener('DOMContentLoaded', () => {
                     type: 'logarithmic',
                     beginAtZero: true,
                     ticks: {
-                        callback: function(value) {
+                        callback: function (value) {
                             return Number(value).toLocaleString();
+                        }
+                    }
+                }
+            },
+            plugins: {
+                legend: {
+                    position: 'top',
+                },
+                title: {
+                    display: true,
+                    text: 'Numbers of Orders & Total Sales',
+                    color: '#00CDBC',
+                    font: {
+                        size: 18
+                    }
+                }
+            }
+        },
+    });
+
+    const ctx2 = document.getElementById('totalSalesChart').getContext('2d');
+    new Chart(ctx2, {
+        type: 'polarArea',
+        data: {
+            labels: labels,
+            datasets: [
+                {
+                    label: 'Total Sales (€)',
+                    data: totalSales,
+                    /* borderColor: 'rgba(255, 99, 132, 1)', */
+                    borderWidth: 1
+                }
+            ]
+        },
+        options: {
+            responsive: true,
+            scales: {
+                y: {
+                    type: 'linear',
+                    beginAtZero: true,
+                }
+            },
+            plugins: {
+                legend: {
+                    position: 'top',
+                },
+                title: {
+                    display: true,
+                    text: 'Total Sales',
+                    color: '#00CDBC',
+                    font: {
+                        size: 18
+                    }
                 }
             }
         }
-    }
-}
+    });
+
+    const ctx3 = document.getElementById('ordersCountChart').getContext('2d');
+    new Chart(ctx3, {
+        type: 'polarArea',
+        data: {
+            labels: labels,
+            datasets: [
+                {
+                    label: 'Numbers of Orders',
+                    data: ordersCount,
+                    /* backgroundColor: Object.values(Utils.CHART_COLORS), */
+                    /* borderColor: 'rgba(255, 99, 132, 1)', */
+                    borderWidth: 1
+                }
+            ]
+        },
+        options: {
+            responsive: true,
+            scales: {
+                y: {
+                    type: 'linear',
+                    beginAtZero: true,
+                }
+            },
+            plugins: {
+                legend: {
+                    position: 'top',
+                },
+                title: {
+                    display: true,
+                    text: 'Number of Orders',
+                    color: '#00CDBC',
+                    font: {
+                        size: 18
+                    }
+                },
+
+            }
+        }
+
     });
 });
+
+
